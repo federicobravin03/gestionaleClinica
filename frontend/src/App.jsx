6f1b53e2de22eb056a2ee8750cac8212cb344f4a
Federico Bravin <federicobravin3@gmail.com>
2026-08-19 22:58:40 +0200

    Aggiunta gestione autenticazione e logout frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,37 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
 
 function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [messaggio, setMessaggio] = useState("");
   const [pazienti, setPazienti] = useState([]);
+  const [autenticato, setAutenticato] = useState(localStorage.getItem("token") !== null)
 
   const eseguiLogin = async () => {
     const risposta = await fetch("http://localhost:8000/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username: username, password: password})
+      body: JSON.stringify({ username: username, password: password })
     });
 
     const dati = await risposta.json();
 
     if (risposta.ok) {
       localStorage.setItem("token", dati.access_token);
+      setAutenticato(true);
       setMessaggio("Login effettuato");
     } else {
-      setMessaggio("Credenziali non valide")
+      setMessaggio("Credenziali non valide");
     }
   }
-  
+
+  const eseguiLogout = () => {
+    localStorage.removeItem("token");
+    setAutenticato(false);
+    setPazienti([]);
+    setMessaggio("");
+  }
+
   const caricaPazienti = async () => {
     const token = localStorage.getItem("token");
 
@@ -36,28 +45,51 @@ function App() {
 
     if (risposta.ok) {
       setPazienti(dati);
-      console.log(dati);
     } else {
       setMessaggio("Errore durante il caricamento")
     }
   }
 
+  useEffect(() => {
+    if(autenticato) {
+      caricaPazienti();
+    }
+  }, [autenticato])
+
   return (
     <div>
       <h1>Gestionale Poliambulatorio</h1>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={eseguiLogin}>Accedi</button>
-      <button onClick={caricaPazienti}>Carica pazienti</button>
+
+      {autenticato ? (
+        <div>
+          <ul>
+            {pazienti.map((paziente) => (
+              <li key={paziente.id}>{paziente.nome} {paziente.cognome}</li>
+            ))}
+          </ul>
+
+          <button onClick = {eseguiLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button onClick={eseguiLogin}>Accedi</button>
+        </div>
+      )}
+
       <p>{messaggio}</p>
+
     </div>
   )
 }
